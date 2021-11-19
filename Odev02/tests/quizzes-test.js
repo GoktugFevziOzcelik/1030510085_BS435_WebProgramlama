075bda091105b26ea992ddda864878aa463b83b4
GoktugFevzi26 <84738270+GoktugFevzi26@users.noreply.github.com>
2021-11-19 11:27:53 +0300

    Delete Odev02 directory

--- Odev02/tests/quizzes-test.js
+++
@@ -1,7 +0,0 @@
-import {Rand} from "../src/kod";
-
-test('gecersiz oyun',()=>{
-    expect(()=>Rand(-1)).toThrow();
-    expect(()=>Rand(0)).toThrow();
-    expect(()=>Rand(99999999)).toThrow();
-})
